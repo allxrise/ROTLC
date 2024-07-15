a86611cad417c2bc82c0efb231dbdf5ba11c7b4a
AllxRise <dahilege@gmail.com>
2024-07-16 00:25:34 +0300

    Updated to 1.19.2

--- src/main/java/me/allxrise/rotlc/mixin/MixinOTLScreen.java
+++ src/main/java/me/allxrise/rotlc/mixin/MixinOTLScreen.java
@@ -1,34 +1,45 @@
 package me.allxrise.rotlc.mixin;
 
-import net.minecraft.client.gui.screen.OpenToLanScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ScreenTexts;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.CyclingButtonWidget;
-import net.minecraft.client.util.NetworkUtils;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.world.GameMode;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.gui.screen.OpenToLanScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.util.NetworkUtils;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.world.GameMode;
+
 @Mixin(OpenToLanScreen.class)
 public abstract class MixinOTLScreen extends Screen {
 
-    @Final
-    @Shadow
-    private static final Text GAME_MODE_TEXT = new TranslatableText("selectWorld.gameMode");
+    @Accessor("ALLOW_COMMANDS_TEXT")
+    public static Text getAllowCommandsText() {
+        throw new AssertionError();
+    }
+    @Accessor("GAME_MODE_TEXT")
+    public static Text getGameModeText() {
+        throw new AssertionError();
+    }
+    @Accessor("OTHER_PLAYERS_TEXT")
+    public static Text getOtherPlayersText() {
+        throw new AssertionError();
+    }
+
     @Mutable
     @Final
     @Shadow
     private final Screen parent;
 
-    private final boolean isNO = false;
+    @Unique
+    private static boolean isNO = false;
+
     @Shadow
     private GameMode gameMode = GameMode.SURVIVAL;
     @Shadow
@@ -45,19 +56,43 @@ public abstract class MixinOTLScreen extends Screen {
         ci.cancel();
     }
 
+    @Unique
     public void addRemovedScreen() {
-        this.addDrawableChild(CyclingButtonWidget.builder(GameMode::getSimpleTranslatableName).values(GameMode.SURVIVAL, GameMode.SPECTATOR, GameMode.CREATIVE, GameMode.ADVENTURE).initially(this.gameMode).build(this.width / 2 - 155, 100, 150, 20, GAME_MODE_TEXT, (button, gameMode) -> {
-            this.gameMode = gameMode;
-        }));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, 100, 150, 20, Text.of("Allow Cheats : NO!"), (button) -> {
-        })).active = false;
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155, this.height - 28, 150, 20, new TranslatableText("lanServer.start"), button -> {
-            this.client.setScreen(null);
+        this.addDrawableChild(CyclingButtonWidget.builder(GameMode::getSimpleTranslatableName)
+                .values(new GameMode[] { GameMode.SURVIVAL, GameMode.SPECTATOR, GameMode.CREATIVE, GameMode.ADVENTURE })
+                .initially(this.gameMode)
+                .build(this.width / 2 - 155, 100, 150, 20, getGameModeText(), (button, gameMode) -> {
+                    this.gameMode = gameMode;
+                }));
+        if (this.isNO) {
+            var dis = new ButtonWidget(this.width / 2 + 5, 100, 150, 20, Text.literal("Haha, NO!"), (b) -> {
+            });
+            dis.active = false;
+            this.addDrawableChild(dis);
+        } else {
+            this.addDrawableChild(
+                    CyclingButtonWidget.onOffBuilder(this.allowCommands).build(this.width / 2 + 5, 100, 150,
+                            20, getAllowCommandsText(), (button, allowCommands) -> {
+                                this.isNO = true;
+                                this.client.setScreen(this.parent);
+                            }));
+        }
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155, this.height - 28, 150, 20,
+                Text.translatable("lanServer.start"), (button) -> {
+            this.client.setScreen((Screen) null);
             int i = NetworkUtils.findLocalPort();
-            TranslatableText text = this.client.getServer().openToLan(this.gameMode, false, i) ? new TranslatableText("commands.publish.started", i) : new TranslatableText("commands.publish.failed");
+            MutableText text;
+            if (this.client.getServer().openToLan(this.gameMode, this.allowCommands, i)) {
+                text = Text.translatable("commands.publish.started", new Object[] { i });
+            } else {
+                text = Text.translatable("commands.publish.failed");
+            }
+
             this.client.inGameHud.getChatHud().addMessage(text);
             this.client.updateWindowTitle();
         }));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height - 28, 150, 20, ScreenTexts.CANCEL, button -> this.client.setScreen(this.parent)));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height - 28, 150, 20, ScreenTexts.CANCEL, (button) -> {
+            this.client.setScreen(this.parent);
+        }));
     }
 }
